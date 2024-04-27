70fb87ba185b585e82222e765df7ac0df3a6bfcc
Muh-Hassk <Muh.alkhuzaee@gmail.com>
2024-04-27 20:07:56 +0300

    Add project files.

---
+++ FirstTask/Controllers/LoginAndRegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTask.Controllers
+{
+    public class LoginAndRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
